be35abe05b6dbcb7a580d07943bd68c74bedc266
maksof-faraz <farazahmedubit12345@gmail.com>
2024-10-27 19:56:36 +0500

    pusihng my work

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
