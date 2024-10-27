a5f71d56177bdef161b5969f48512bb87ab89d8c
maksof-faraz <farazahmedubit12345@gmail.com>
2024-10-27 20:03:12 +0500

    push changess

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Hello, Worldddd!');
+  res.send(`Hello, Worldddd! ${PORT}`);
 });
 
 // Start the server
